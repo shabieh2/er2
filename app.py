4c4931e00af917449c6d6c87b944caf8c9236edf
shabieh2 <shabieh2@gmail.com>
2022-03-03 22:16:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -169,19 +169,7 @@ if prediction_mode == 'Image':
 
 elif prediction_mode == 'Realtime Stream':
     
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-    ret, frame = cap.read()
-    
-    # Make detections 
-    results = model(frame)
-    
-    cv2.imshow('YOLO', np.squeeze(results.render()))
-    
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-    cap.release()
-    cv2.destroyAllWindows()
+   
     
     
     ctx = webrtc_streamer(
